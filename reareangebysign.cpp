5eceb73f5371827493b8caac26d13bec23a5ac0d
Willem van der Vorst <willemvdvorst@gmail.com>
2024-02-14 11:23:38 +0100

    gumo

---
+++ reareangebysign.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <iostream>
+
+std::vector<int> rearrangeArray(std::vector<int>& nums) {
+	std::vector<int> pos;
+	std::vector<int> neg;
+
+	for (auto nb : nums)
+	{
+		if (nb < 0)
+			neg.push_back(nb);
+		else
+			pos.push_back(nb);
+
+	}
+	int j = 0;
+	for (int i = 0; i < nums.size(); i++)
+	{
+		if (i % 2 == 0)
+			nums[i] = pos[j];
+		else
+		{
+			nums[i] = neg[j];
+			j++;
+		}
+	}
+	return (nums);
+}
+
+int main()
+{
+	std::vector<int> nums = {3, 1, -2, -5, 2, -4};
+	rearrangeArray(nums);
+}
