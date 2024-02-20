23fc165658e94fca19ff0b1e75e7252a948c7192
Willem van der Vorst <willemvdvorst@gmail.com>
2024-02-20 13:03:40 +0100

    gumo2

--- missingNumber.cpp
+++ missingNumber.cpp
@@ -1,14 +1,25 @@
-class Solution {
-public:
-    int missingNumber(vector<int>& nums) {
-        int n = nums.size();
+#include <vector>
+#include <iostream>
 
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < n; i++)
-        {
-            if (i != nums[i])
-                return (i);
-        }
-        return (n);
-    }
-};
+int missingNumber(std::vector<int>& nums) {
+	int n = nums.size();
+	std::vector<int> res(n + 1, -1);
+
+	for(int i = 0; i < n; i++)
+	{
+		res[nums[i]] = nums[i];
+	}
+
+	for(int i = 0; i <= n; i++)
+	{
+		if (res[i] == -1)
+			return (i);
+	}
+	return (-1);
+}
+
+int main()
+{
+	std::vector<int> nums = {3, 0, 1};
+	std::cout << missingNumber(nums) << std::endl;
+}
