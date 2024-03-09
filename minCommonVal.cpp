1ded273c7f6430fc596cec217deed477e36dedce
Willem van der Vorst <willemvdvorst@gmail.com>
2024-03-09 12:38:29 +0100

    hi

---
+++ minCommonVal.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int getCommon(vector<int>& nums1, vector<int>& nums2) {
+	int i = 0;
+	int j = 0;
+
+	while (i < nums1.size() && j < nums2.size())
+	{
+		if (nums1[i] == nums2[j])
+			return nums1[i];
+		else if (nums1[i] < nums2[j])
+			i++;
+		else
+			j++;
+	}
+	return -1;
+}
+
+int main()
+{
+	vector<int> nums1 = {1, 2, 3};
+	vector<int> nums2 = {2, 4};
+	cout << getCommon(nums1, nums2) << endl;
+}
---
+++ sameTree.cpp
@@ -0,0 +1,12 @@
+
+
+bool isSameTree(TreeNode* p, TreeNode* q) {
+	if (!p && !q)
+		return (true);
+	if (!p || !q)
+		return (false);
+	if (p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right))
+		return (true);
+	else
+		return (false);
+}
